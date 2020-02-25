85cb773f9af7e59c771aa6c20c5d8568c51620bf
Krystek <krystianlenczewski2@gmail.com>
2020-02-25 13:41:31 +0100

    making user profile

---
+++ NewsForums.Data/IUpload.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NewsForums.Data
+{
+    public interface IUpload
+    {
+    }
+}
---
+++ NewsForums/Controllers/ProfileController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NewsForums.Data;
+using NewsForums.Data.Models;
+
+namespace NewsForums.Controllers
+{
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IApplicationUser _userService;
+        private readonly IUpload _uploadService;
+        public ProfileController(UserManager<ApplicationUser>userManager,
+           IApplicationUser userService )
+        {
+            _userManager = userManager;
+            _userService = userService;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Detail(string id)
+        {
+            
+            return View();
+        }
+    }
+}
